8fc0039857cd55f1a5846e3cb04e8db6b84b47a9
Rubóczki Benjámin <h773772@stud.u-szeged.hu>
2024-05-11 21:58:56 +0200

    registration layout - update

--- app/src/main/java/com/example/massageapplication/MainActivity.java
+++ app/src/main/java/com/example/massageapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.massageapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,10 +15,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
     private static final int SECRET_KEY = 99;
 
     EditText userNameET;
     EditText passwordET;
+    private SharedPreferences preferences;
 
 
     @Override
@@ -29,11 +32,15 @@ public class MainActivity extends AppCompatActivity {
          userNameET = findViewById(R.id.editTextUserName);
          passwordET = findViewById(R.id.editTextPassword);
 
+         preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public void login(View view) {
@@ -42,12 +49,52 @@ public class MainActivity extends AppCompatActivity {
         String userName = userNameET.getText().toString();
         String password = passwordET.getText().toString();
 
-        Log.i(LOG_TAG,"Bejelentkezett: "+ userName+ ", jelszó: "+ password);
+        Log.i(LOG_TAG,"Bejelentkezett: " + userName+ ", jelszó: "+ password);
     }
 
     public void register(View view) {
         Intent registration = new Intent(this, RegisterActivity.class);
-        registration.putExtra("SECRET_KEY", 99);
+        registration.putExtra("SECRET_KEY", SECRET_KEY);
         startActivity(registration);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userNameET.getText().toString());
+        editor.putString("password", passwordET.getText().toString());
+        editor.apply();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(LOG_TAG, "onRestart");
+    }
+
 }
--- app/src/main/java/com/example/massageapplication/RegisterActivity.java
+++ app/src/main/java/com/example/massageapplication/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.massageapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -11,12 +13,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
     EditText userNameEditText;
     EditText userEmailEditText;
     EditText passwordEditText;
     EditText passwordConfirmationEditText;
+    EditText phoneEditText;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +39,23 @@ public class RegisterActivity extends AppCompatActivity {
         userEmailEditText = findViewById(R.id.userEmailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         passwordConfirmationEditText = findViewById(R.id.passwordConfirmationEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password","");
+
+        userNameEditText.setText(userName);
+        passwordEditText.setText(password);
+
+        Log.i(LOG_TAG, "onCreate");
     }
 
     public void register(View view) {
@@ -46,6 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
         String email = userEmailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
         String passwordConfirmation = passwordConfirmationEditText.getText().toString();
+        String phone = phoneEditText.getText().toString();
 
         if(!password.equals(passwordConfirmation)) Log.e(LOG_TAG,"Nem egyforma jelszót adtál meg.");
 
@@ -54,5 +72,45 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void cancel(View view) {
         finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        //TODO
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+    //TODO
     }
 }
