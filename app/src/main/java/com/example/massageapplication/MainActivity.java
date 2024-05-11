fa0681220ea747e77c203aea1d3d9c6ddefd1511
Rubóczki Benjámin <h773772@stud.u-szeged.hu>
2024-05-11 18:09:53 +0200

    registration activity

--- app/src/main/java/com/example/massageapplication/MainActivity.java
+++ app/src/main/java/com/example/massageapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.massageapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,12 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
+    private static final int SECRET_KEY = 99;
+
+    EditText userNameET;
+    EditText passwordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+         userNameET = findViewById(R.id.editTextUserName);
+         passwordET = findViewById(R.id.editTextPassword);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,12 +37,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        EditText userName = findViewById(R.id.editTextUserName);
-        EditText password = findViewById(R.id.editTextPassword);
 
-        String userNameStr = userName.getText().toString();
-        String passwordStr = password.getText().toString();
 
-        Log.i(LOG_TAG,"Bejelentkezett: "+ userNameStr+ ", jelszó: "+ passwordStr);
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG,"Bejelentkezett: "+ userName+ ", jelszó: "+ password);
+    }
+
+    public void register(View view) {
+        Intent registration = new Intent(this, RegisterActivity.class);
+        registration.putExtra("SECRET_KEY", 99);
+        startActivity(registration);
     }
 }
