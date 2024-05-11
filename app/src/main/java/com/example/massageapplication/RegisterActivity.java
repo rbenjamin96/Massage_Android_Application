3d544cb75c612bc59416a9ca0253b4ae41c47d9b
Rubóczki Benjámin <h773772@stud.u-szeged.hu>
2024-05-11 19:05:22 +0200

    registration layout

--- app/src/main/java/com/example/massageapplication/RegisterActivity.java
+++ app/src/main/java/com/example/massageapplication/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.massageapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText passwordEditText;
+    EditText passwordConfirmationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +29,30 @@ public class RegisterActivity extends AppCompatActivity {
         int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
 
         if (secret_key != 99) finish();
+        userNameEditText = findViewById(R.id.userNameEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordConfirmationEditText = findViewById(R.id.passwordConfirmationEditText);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void register(View view) {
+        String userName = userNameEditText.getText().toString();
+        String email = userEmailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordConfirmation = passwordConfirmationEditText.getText().toString();
+
+        if(!password.equals(passwordConfirmation)) Log.e(LOG_TAG,"Nem egyforma jelszót adtál meg.");
+
+        Log.i(LOG_TAG,"Regisztrált: "+ userName+ ", email: "+ email);
+    }
+
+    public void cancel(View view) {
+        finish();
     }
 }
