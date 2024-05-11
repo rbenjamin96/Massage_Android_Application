3c55fc7552207418a0dbf1dbe3aafe336dc898ed
Rubóczki Benjámin <h773772@stud.u-szeged.hu>
2024-05-11 16:49:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/massageapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.massageapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login(View view) {
+        EditText userName = findViewById(R.id.editTextUserName);
+        EditText password = findViewById(R.id.editTextPassword);
+
+        String userNameStr = userName.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        Log.i(LOG_TAG,"Bejelentkezett: "+ userNameStr+ ", jelszó: "+ passwordStr);
+    }
+}
